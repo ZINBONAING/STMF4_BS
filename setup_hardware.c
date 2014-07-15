9dae3e811621d0b67989aa7d68f2b7a76a211abb
ZINBONAING <zinbonaing@gmail.com>
2014-07-16 01:15:40 +0800

    3DM_T1

--- setup_hardware.c
+++ setup_hardware.c
@@ -15,6 +15,11 @@
 #include <stm32f4xx_it.h>
 #include <Global_variables.h>
 int Buf_counter=0;
+int expect_received=0,receivedmsg[20],received_msg=0,bufcount=0;
+
+
+
+
 
 
 void init_USART3(uint32_t baudrate){
@@ -84,6 +89,78 @@ void init_USART3(uint32_t baudrate){
 
 }
 
+
+//----------------------------UART 4 for 3DM----------------------------------------------------------
+void init_USART2(uint32_t baudrate){
+
+	/* This is a concept that has to do with the libraries provided by ST
+	 * to make development easier the have made up something similar to
+	 * classes, called TypeDefs, which actually just define the common
+	 * parameters that every peripheral needs to work correctly
+	 *
+	 * They make our life easier because we don't have to mess around with
+	 * the low level stuff of setting bits in the correct registers
+	 */
+	GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
+	USART_InitTypeDef USART_InitStruct; // this is for the USART1 initilization
+	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
+
+	/* enable APB2 peripheral clock for USART1
+	 * note that only USART1 and USART6 are connected to APB2
+	 * the other USARTs are connected to APB1
+	 */
+	/* --------------------------- System Clocks Configuration -----------------*/
+	  /* USART3 clock enable */
+	  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	  /* GPIOB clock enable */
+	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	/* This sequence sets up the TX and RX pins
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+  /*-------------------------- GPIO Configuration ----------------------------*/
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* Connect USART pins to AF */
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource5, GPIO_AF_USART2);
+  GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART2);
+
+	/* Now the USART_InitStruct is used to define the
+	 * properties of USART1
+	 */
+    USART_InitStruct.USART_BaudRate = 38400;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+
+    USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+    USART_Init(USART2, &USART_InitStruct);
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE); // enable the USART1 receive interrupt
+
+        	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;		 // we want to configure the USART1 interrupts
+        	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 4;// this sets the priority group of the USART1 interrupts
+        	NVIC_InitStructure.NVIC_IRQChannelSubPriority =0;		 // this sets the subpriority inside the group
+        	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			 // the USART1 interrupts are globally enabled
+        	NVIC_Init(&NVIC_InitStructure);							 // the properties are passed to the NVIC_Init function which takes care of the low level stuff
+    USART_Cmd(USART2, ENABLE);
+
+
+
+
+
+}
+
+
+
+//----------------------------END UART4 for 3DM-------------------------------------------------------
 /* This function is used to transmit a string of characters via
  * the USART specified in USARTx.
  *
@@ -209,6 +286,41 @@ void USART3_IRQHandler(void){
 
         }
 */
+		}
+
+
+	}
+void USART2_IRQHandler(void){
+
+	// check if the USART4 receive interrupt flag was set
+	if( USART_GetITStatus(USART2, USART_IT_RXNE) ){
+
+		static uint8_t cnt = 0; // this counter is used to determine the string length
+		char t = USART2->DR; // the character from the USART1 data register is saved in t
+
+		if(expect_received==1){
+			if(t == 0x0E ){
+			 bufcount=1;
+			 receivedmsg[bufcount]=t;
+			}
+			expect_received=2;
+
+		}
+		if(expect_received==2){
+			bufcount=bufcount+1;
+			receivedmsg[bufcount]=t;
+
+		}
+
+		if(bufcount>10){
+
+			expect_received==0;
+			received_msg=1;
+
+		}
+
+
+
 		}
 
 
